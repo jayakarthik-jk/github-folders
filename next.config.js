217d57911ceeb10092052638d9a68112b8e51c97
jayakarthik-jk <karthick72002@gmail.com>
2023-07-07 00:59:58 +0530

    enabled strict mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   publicRuntimeConfig: {
     supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
     supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
