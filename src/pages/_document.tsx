e4a292cc84aac1bb4024704a1c76dce5f7a3a8b0
jayakarthik-jk <karthick72002@gmail.com>
2023-07-16 14:15:26 +0530

    removed unused imports

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { type FC } from "react";
-import NextTopLoader from "nextjs-toploader";
 
 const Document: FC = () => {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <NextTopLoader />
         <Main />
         <NextScript />
       </body>
